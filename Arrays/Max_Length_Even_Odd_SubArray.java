7dcc09e70a5cfd108e0f67a99aade14cc84e3fe8
jaythanki18 <21CE143@charusat.edu.in>
2023-01-03 14:20:07 +0530

    Maximum odd even subarray

--- Arrays/Max_Length_Even_Odd_SubArray.java
+++ Arrays/Max_Length_Even_Odd_SubArray.java
@@ -5,7 +5,7 @@ public class Max_Length_Even_Odd_SubArray {
         int res=1;
         int curr=1;
         for(int i=1;i<n;i++){
-            if((arr[i]%2==0 && arr[i-1]%2!=0)|| (arr[i]%2!=0 && arr[i-1]==0)){
+            if((arr[i]%2==0 && arr[i-1]%2!=0)|| (arr[i]%2!=0 && arr[i-1]%2==0)){
                 curr++;
                 res=Math.max(res,curr);
             }
