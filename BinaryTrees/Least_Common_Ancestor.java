66e7664428627d4d25379df2baaf1ff3a9264bd2
jaythanki18 <21CE143@charusat.edu.in>
2023-03-17 01:23:27 +0530

    Find least common ancestor of the tree

--- BinaryTrees/Least_Common_Ancestor.java
+++ BinaryTrees/Least_Common_Ancestor.java
@@ -46,6 +46,23 @@ public class Least_Common_Ancestor {
         Node lca=path1.get(i-1);
         return lca;
     }
+
+    public static Node lca2(Node root,int n1,int n2){
+        if(root==null || root.data==n1 || root.data==n2){
+            return root;
+        }
+
+        Node leftLca=lca2(root.left,n1,n2);
+        Node rightLca=lca2(root.right,n1,n2);
+
+        if(rightLca==null){
+            return leftLca;
+        }
+        if(leftLca==null){
+            return rightLca;
+        }
+        return root;
+    }
     public static void main(String[] args) {
         Node root=new Node(1);
         root.left=new Node(2);
@@ -55,6 +72,6 @@ public class Least_Common_Ancestor {
         root.right.left=new Node(6);
         root.right.right=new Node(7);
         int n1=4,n2=5;
-        System.out.println(lca(root,n1,n1).data);
+        System.out.println(lca2(root,n1,n2).data);
     }
 }
