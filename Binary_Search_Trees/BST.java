e070950b3643faf3724ad6b408b855e886beb1fe
jaythanki18 <21CE143@charusat.edu.in>
2023-03-20 22:09:48 +0530

    Binary Search Tree

--- Binary_Search_Trees/BST.java
+++ Binary_Search_Trees/BST.java
@@ -4,6 +4,8 @@
 
 package Binary_Search_Trees;
 
+import java.util.ArrayList;
+
 public class BST {
     static class Node{
         int data;
@@ -86,6 +88,24 @@ public class BST {
         return root;
     }
 
+    public static void printPath(ArrayList<Integer> path){
+        for(int i=0;i<path.size();i++){
+            System.out.print(path.get(i)+"->");
+        }
+        System.out.println("N");
+    }
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
+        if(root==null){
+            return;
+        }
+        path.add(root.data);
+        if(root.left==null && root.right==null){
+            printPath(path);
+        }
+        printRoot2Leaf(root.left,path);
+        printRoot2Leaf(root.right,path);
+        path.remove(path.size()-1);
+    }
     public static void main(String[] args) {
         int values[]={8,5,3,1,4,6,10,11,14};
         Node root=null;
@@ -104,7 +124,8 @@ public class BST {
 //            System.out.println("not found");
 //        }
 
-        root=delete(root,1);
+        //root=delete(root,1);
+        printRoot2Leaf(root,new ArrayList<>());
         System.out.println();
     }
 }
